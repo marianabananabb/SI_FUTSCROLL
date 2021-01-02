fdb1b983f9977b5ca77a017497f5e20f9ebc7f7b
marianabananabb <76714111+marianabananabb@users.noreply.github.com>
2021-01-02 01:49:03 +0000

    Add files via upload

---
+++ projeto-SI/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" initial-scale=1, height=device-height">
+    <title>FUTSCROLL</title>
+</head>
+<body>
+
+
+<header>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+        <li><a href="Melhores_marcadores.php">TABELA CLASSIFICATIVA
+            <p>PRIMEIRA LIGA</p>
+            <P>MELHORES MARCADORES</P></a></li>
+        <li><a href="Equipas.php">PROJECTS
+            <P>RESUMO DE JOGOS</P></a></li>
+        <li><a href="Jornadas.php">JORNADAS </a></li>
+        <li><a href="Calendário.php">CALENDÁRIO</a></li>
+    </ul>
+    <a href="index.php" class="logo">
+        <h1>FUTSCROLL</h1>
+    </a>
+
+    <a href="Login_Administrador" class="logo">
+    <h2>ADMIN</h2>
+    </a>
+
+
+
+
+</header>
+
+
+<main>
+
+<div>
+    <h1>HOME</h1>
+
+    <img src="Capturar.png" alt="Logo"/>
+
+</div>
+
+
+<footer>
+
+    <div class="footer-info">
+        <ul>
+            <li><a href="#">PRIVACY POLICY</a></li>
+            <li><a href="#">TERMS AND CONDITIONS</a></li>
+        </ul>
+    </div>
+    <div class="footer-social">
+        <a href="https://www.facebook.com/"><img src="img/facebook.png" alt="Link to facebook"></a>
+        <a href="https://www.instagram.com/"><img src="img/instagram.png" alt="Link to instagram"></a>
+        <a href="https://www.twitter.com/"><img src="img/tt.png" alt="Link to twitter"></a>
+    </div>
+
+
+
+
+</footer>
+
+
+
+</body>
+</html>
+<?php
